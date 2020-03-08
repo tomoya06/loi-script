30ff6c1279710d55bfad9584a30ec011189a568f
tomoya06 <peng4766@gmail.com>
2020-03-08 15:23:46 +0800

    definition regexp works

--- transfer/index.js
+++ transfer/index.js
@@ -9,11 +9,18 @@ class PEG {
   def = "";
 }
 
+class DEF {
+  word = "";
+  rawId = "";
+  numId = "";
+  py = "";
+  mean = "";
+}
+
 class Word {
   word = "";
-  pys = [];
-  mpys = [];
-  def = "";
+  alias = "";
+  def = [];
   egs = "";
   pegs = [];
   cmts = [];
@@ -23,6 +30,9 @@ const CN_CHAR_REG = /[\u4e00-\u9fa5]/;
 const IDX = ['①', '②', '③', '④', '⑤', '⑥',];
 const IDX_REG = /[①②③④⑤⑥]/
 
+const MULTI_SPELL_REG = /^[\u4e00-\u9fa5](\/[\u4e00-\u9fa5])+/;
+const DEF_REG = /^((?:[\u4e00-\u9fa5](?:\/[\u4e00-\u9fa5])*){0,1})([①②③④⑤⑥]{0,1})((?:\[[\w\W]+\])+)(.*)/;
+
 async function process() {
   const words = await connection.query('SELECT DISTINCT 字形 FROM 表1');
   console.log(`read ${words.length} word`);
@@ -30,80 +40,32 @@ async function process() {
   const docs = [];
 
   for (let i = 0; i < words.length; i += 1) {
-    const word = words[i];
-    const defines = await connection.query(`SELECT * FROM 表1 WHERE 字形=${word}`);
+    const curWord = words[i];
+    const defines = await connection.query(`SELECT * FROM 表1 WHERE 字形=${curWord}`);
 
     const newWord = new Word();
 
-    const pinyins = defines.map(define => define['拼音']);
     const define = defines[0]['音义辨析'];
     const examples = define['例'];
     const refer = define['《康熙字典》等典籍摘录'];
     const oldPinyin = define['《广韵》字音'];
 
-    // try {
-    //   if (define.contains(IDX[0])) {
-    //     // 有多组读音
-    //     const DEFINE_PINYIN_REG = /[①②③④⑤⑥](\[[\w\d]+\])+/g;
-    //     const mappedPinyins = define.match(DEFINE_PINYIN_REG)
-    //       .map(py => py.slice(1).match(/\[[\w\d]+\]/).map(py => py.replace(/[\[\]]/g, '')));
+    const pinyins = [];
+   
+    const defines = define.split('##');
+    const defineAlias = defines[0];
+    const mappedSubDefines = defines.slice(1).map(subDefine => {
+      const _def = new DEF();
 
-    //     newWord.mpys = mappedPinyins;
-    //   } else {
-    //     // 只有一组读音
-    //     const DEFINE_PINYIN_REG = /(\[[\w\d]+\])+/g;
-    //     const mappedPinyins = define.match(DEFINE_PINYIN_REG).map(py => py.replace(/[\[\]]/g, '')));
-    //     newWord.mpys = [mappedPinyins];
-    //   }
-    // } catch (error) {
-    //   console.log(`cant parse pinyin for ${word}`);
-    // }
+      const regResult = subDefine.match(DEF_REG);
+      _def.word = regResult[1] ? regResult[1] : defineAlias;
+      _def.rawId = `${regResult[1]}${regResult[2]}`;
+      const numberIdx = regResult[2] ? IDX.indexOf(regResult[2]) : 0;
+      _def.numId = `${regResult[1]}${numberIdx}`;
+      _def.py = regResult[3];
+      _def.mean = regResult[4];
 
-    if (!!examples && examples.contains('【')) {
-      // 有例词
-      try {
-        if (examples.contains(IDX[0])) {
-          // 例子对应多组读音
-          const exampleGroups = examples.split(/(?=[①②③④⑤⑥])/).filter(e => !!e);
-          for (let gi = 0; gi < exampleGroups.length; gi += 1) {
-            const group = exampleGroups[gi].replace(/[①②③④⑤⑥]：?/, '');
-            const groupPinyinIndex = IDX.indexOf(exampleGroups[gi][0]);
-
-            const groupExampleList = group
-              .split(/#+/)
-              .filter(e => !!e).map(example => {
-                const [exampleWord, exampleDefine] = example.slice('【'.length).split('】');
-                const cnPinyinReg = /[\u4e00-\u9fa5](\[[\w\d]+\])?/g;
-                const splitedWord = exampleWord.match(cnPinyinReg);
-                const ogExampleWord = splitedWord.map(w => w.match(/[\u4e00-\u9fa5]/)[0]);
-                // const ogExampleWordPinyin = [];
-                // for (let wi = 0; wi < splitedWord.length; wi += 1) {
-                //   const pyForWordWi = splitedWord[wi].match(/\[[\w\d]+\]/);
-                //   if (!!pyForWordWi) {
-                //     ogExampleWordPinyin[wi] = pyForWordWi;
-                //   } else {
-                //     if (!!newWord.mpys) {
-                //       if (wi === 0) {
-                //         if (ogExampleWord[wi] === word) {
-                //           ogExampleWordPinyin[wi] = newWord.mpys[gi];
-                //         }
-                //       }
-                //     }
-                //   }
-                // }
-              });
-
-          }
-        } else {
-          // 例子只有一组读音
-
-        }
-      } catch (error) {
-        console.log(`cant parse examples for ${word}`);
-      }
-    } else {
-      // 没有例子
-      console.log(`examples for ${word} is not in format`);
-    }
+      return _def;
+    })
   }
 }
